e9aa62037ad11aed8f5a4e3c8081e812ca15a060
Freiii21 <mrfreiii@gmail.com>
2022-01-23 22:09:46 +0300

    #42 portfolio_1

---
+++ src/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Nav } from '../nav/Nav';
+import s from './Header.module.css'
+
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <Nav />
+        </div>
+    );
+}
