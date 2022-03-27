161272aacaf81056f3c65f386ab38a3655bb99bb
Freiii21 <mrfreiii@gmail.com>
2022-03-28 01:18:23 +0300

    #42 design improvements_4

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -11,11 +11,12 @@ export const Skills = () => {
                 <Title value={"Skills"}/>
                 <div className={s.skillsSet}>
                     <Skill title='JS' description='Some description...'/>
-                    <Skill title='CSS' description='Some description with a loooooong string...'/>
-                    <Skill title='REACT' description='Some description...'/>
                     <Skill title='TypeScript' description='Some description...'/>
+                    <Skill title='React' description='Some description...'/>
                     <Skill title='Redux' description='Some description...'/>
                     <Skill title='REST API' description='Some description...'/>
+                    <Skill title='CSS' description='Some description with a loooooong string...'/>
+                    <Skill title='SASS' description='Some description with a loooooong string...'/>
                 </div>
             </div>
         </div>
