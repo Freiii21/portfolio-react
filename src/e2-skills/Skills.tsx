76dda60ee2587874b1b80da3ace4201dee9a3201
Freiii21 <mrfreiii@gmail.com>
2022-03-30 23:40:10 +0300

    #42 design improvements_10

--- src/e2-skills/Skills.tsx
+++ src/e2-skills/Skills.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Skills.module.css'
+import s from './Skills.module.scss'
 import sc from './../common/styles/Container.module.scss'
 import {Skill} from './skill/Skill';
 import {Title} from '../common/components/Title';
